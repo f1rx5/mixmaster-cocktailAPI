73823f2872790ac6e69a5321a9303ee5b1f56fdf
Firas Sharief <95207263+f1rx5@users.noreply.github.com>
2023-11-16 16:15:47 +0530

    v1

---
+++ src/pages/SinglePageError.jsx
@@ -0,0 +1,10 @@
+import { useRouteError } from "react-router-dom"
+
+const SinglePageError = () => {
+    const error = useRouteError()
+    console.log(error);
+  return (
+    <h2>There was an error...</h2>
+  )
+}
+export default SinglePageError
